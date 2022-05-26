cdbbe60dc7a48f5f21f5983513d62a15385b9774
fbamuse <fukuhara.masanori@gmail.com>
2022-05-26 19:29:07 +0900

    first commit

--- lesson-2-joins-and-json/exercises/starter/bank-deposits.py
+++ lesson-2-joins-and-json/exercises/starter/bank-deposits.py
@@ -1,30 +1,47 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, to_json, col, unbase64, base64, split, expr
 from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
+# this is a manually created schema - before Spark 3.0.0, schema inference is not automatic
+# since we are not using the date or the amount in sql calculations, we are going
+# to cast them as strings
+kafkaMessageSchema = StructType (
+    [
+        StructField("accountNumber", StringType()),
+        StructField("amount", StringType()),
+        StructField("dateAndTime", StringType())
+    ]
+    
+)
 
-# TO-DO: create a kafka message schema StructType including the following JSON elements:
-# {"accountNumber":"703934969","amount":415.94,"dateAndTime":"Sep 29, 2020, 10:06:23 AM"}
+# the source for this data pipeline is a kafka topic, defined below
+spark = SparkSession.builder.appName("bank-deposits").getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
 
-# TO-DO: create a spark session, with an appropriately named application name
+bankDepositsRawStreamingDF = spark                          \
+    .readStream                                          \
+    .format("kafka")                                     \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe","bank-deposits")                  \
+    .option("startingOffsets","earliest")\
+    .load()                                     
 
-#TO-DO: set the log level to WARN
+#it is necessary for Kafka Data Frame to be readable, to cast each field from a binary to a string
+bankDepositsStreamingDF = bankDepositsRawStreamingDF.selectExpr("cast(key as string) key", "cast(value as string) value")
 
-#TO-DO: read the atm-visits kafka topic as a source into a streaming dataframe with the bootstrap server kafka:19092, configuring the stream to read the earliest messages possible                                    
+# this creates a temporary streaming view based on the streaming dataframe
+# it can later be queried with spark.sql, we will cover that in the next section 
+bankDepositsStreamingDF.withColumn("value",from_json("value",kafkaMessageSchema))\
+        .select(col('value.*')) \
+        .createOrReplaceTempView("BankDeposits")
 
-#TO-DO: using a select expression on the streaming dataframe, cast the key and the value columns from kafka as strings, and then select them
+# Using spark.sql we can select any valid select statement from the spark view
+bankDepositsSelectStarDF=spark.sql("select * from BankDeposits")
 
-#TO-DO: using the kafka message StructType, deserialize the JSON from the streaming dataframe 
-
-# TO-DO: create a temporary streaming view called "BankDeposits" 
-# it can later be queried with spark.sql
-
-#TO-DO: using spark.sql, select * from BankDeposits
-
-# TO-DO: write the stream to the console, and configure it to run indefinitely, the console output will look something like this:
+# this takes the stream and "sinks" it to the console as it is updated one message at a time:
 # +-------------+------+--------------------+
 # |accountNumber|amount|         dateAndTime|
 # +-------------+------+--------------------+
 # |    103397629| 800.8|Oct 6, 2020 1:27:...|
 # +-------------+------+--------------------+
 
-
+bankDepositsSelectStarDF.writeStream.outputMode("append").format("console").start().awaitTermination()
